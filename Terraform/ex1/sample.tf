621f2be3250e1f629bb76a84c27541a89fa8cb02
skatam <smaar.katam@gmail.com>
2023-07-04 23:34:01 +0800

    terraform testing

--- Terraform/ex1/sample.tf
+++ Terraform/ex1/sample.tf
@@ -3,15 +3,22 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Web"
   }
 }
+output "web_ipaddress" {
+value = aws_instance.web.public_ip
+}
+
 
 resource "aws_instance" "user" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "user"
   }
+}
+output "user_ipaddress" {
+value = aws_instance.user.public_ip
 }
