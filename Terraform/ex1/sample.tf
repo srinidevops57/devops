ec028637ed7df220211e20cef232203480af1219
skatam <smaar.katam@gmail.com>
2023-07-04 23:50:58 +0800

    terraform testing

--- Terraform/ex1/sample.tf
+++ Terraform/ex1/sample.tf
@@ -6,7 +6,7 @@ data "aws_ami" "centos" {
 
 
 resource "aws_instance" "web" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -19,7 +19,7 @@ value = aws_instance.web.public_ip
 
 
 resource "aws_instance" "user" {
-  ami           = "data.aws_ami.centos.image_id"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
